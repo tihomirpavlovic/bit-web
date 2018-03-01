8be57da5290d9d8b6edfd32caeaaf78ee2696e46
BIT Student <classroom@bgit.rs>
2018-03-01 17:54:51 +0100

    mario

---
+++ MarioProject/JS/mario.js
@@ -0,0 +1,11 @@
+
+document.querySelector("body").addEventListener("keydown", function(event){
+
+    if( event.keyCode === 39){
+        document.querySelector("#stoji").classList.add("display");
+        document.querySelector(".ide").classList.add("ide");
+    }
+
+
+
+});
